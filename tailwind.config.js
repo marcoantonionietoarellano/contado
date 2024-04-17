f641d071bc6d30277775ed6877a5e6c917621b78
marcoantonionietoarellano <138030356+marcoantonionietoarellano@users.noreply.github.com>
2024-04-17 08:22:21 -0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/**  @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/*/.{html,js,tsx}',
+  './pages/*/.{html,js}',
+  './index.html',],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
